6afdd44f846f6fc03181e64bc31d307c16595379
Jan Schwenzien <jan@general-scripting.com>
2017-09-14 21:55:18 +0200

    added debug prints to websocker broadcaster

--- backend/ws_broadcaster.py
+++ backend/ws_broadcaster.py
@@ -4,15 +4,18 @@ clients = []
 class Broadcaster(WebSocket):
 
     def handleMessage(self):
+       print('message:', self.data)
        for client in clients:
           if client != self:
              client.sendMessage(self.data)
 
     def handleConnected(self):
+       print(self.address, 'connected')
        clients.append(self)
 
     def handleClose(self):
        clients.remove(self)
+       print(self.address, 'disconnected')
 
 server = SimpleWebSocketServer('', 8000, Broadcaster)
 server.serveforever()

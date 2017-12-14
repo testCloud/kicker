0212f2cb7f615e9fd15ff49f80f0de32b8f4b1c3
Katter <katter@test.io>
2017-12-14 20:45:07 +0000

    re-enable yellow IR wiretrap and stuff

--- frontend/js/script_xmas.js
+++ frontend/js/script_xmas.js
@@ -1,33 +1,19 @@
 var socket = new WebSocket('ws://kicker.local:15674');
 
-// Connection opened
+/*
 socket.addEventListener('open', function (event) {
-    //socket.send('Hello Server!');
     console.log("opened!");
 });
+*/
 
 // Listen for messages
 socket.addEventListener('message', function (message) {
     window.rstate.states = JSON.parse(message.data);
 });
 
+/*
 $(document).on('click', '#score_resetter_button', function(e) {
   socket.send('{ "event": "reset" }');
 });
-
-
-/*
-var client = Stomp.over(ws);
-
-var on_connect = function() {
-    console.log("HERE");
-    var sub = client.subscribe('/exchange/kicker', function(message) {
-        console.log("WebWorker: " + message.body);
-        window.bstate.states = JSON.parse(message.body).payload;
-    });
-};
-var on_error =  function() {
-    console.log('error');
-};
-client.connect(null, null, on_connect, on_error, '/');
 */
+

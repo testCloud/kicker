8688861a1f8412983ff4d1a4622bb0aed22a42f7
Katter <katter@test.io>
2018-02-13 21:12:08 +0000

    WIP splitting app into different concerns

---
+++ backend/jan_itor.py
@@ -0,0 +1,33 @@
+import time
+from settings import Settings
+from RPi import GPIO
+from socket_publisher import SocketPublisher
+
+if __name__ == '__main__':
+    socket_publisher = SocketPublisher()
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    GPIO.setup(Settings.IR_BLACK, GPIO.IN)
+    GPIO.setup(Settings.IR_YELLOW, GPIO.IN)
+
+    timer_in_seconds = 1200
+    stop_after = time.time()
+    ready_for_new_goal = True
+
+    while True:
+        if ready_for_new_goal:
+            if GPIO.input(Settings.IR_BLACK) == 1:
+                stop_after = time.time() + timer_in_seconds
+                ready_for_new_goal = False
+                socket_publisher.publish({ 'message': 'goal', 'payload': { 'team': 'black' } })
+
+            if GPIO.input(Settings.IR_YELLOW) == 1 :
+                stop_after = time.time() + timer_in_seconds
+                ready_for_new_goal = False
+                socket_publisher.publish({ 'message': 'goal', 'payload': { 'team': 'yellow' } })
+
+        if time.time() - stop_after + timer_in_seconds > 1:
+            ready_for_new_goal = True
+        time.sleep(.01)
